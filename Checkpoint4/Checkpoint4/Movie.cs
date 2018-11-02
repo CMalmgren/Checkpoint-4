5ea5b452b706763ebaf8ae67045e0027a6d4e9cc
Caroline Malmgren <caroline.malmgren@live.se>
2018-11-02 15:12:33 +0100

    Added class Movie, and sorting and printing each movie from a list

---
+++ Checkpoint4/Checkpoint4/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkpoint4
+{
+    class Movie
+    {
+        public string Name { get; set; }
+    }
+}
--- Checkpoint4/Checkpoint4/Program.cs
+++ Checkpoint4/Checkpoint4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Checkpoint4
 {
@@ -11,7 +13,15 @@ namespace Checkpoint4
             Console.WriteLine("Best movies ever!");
             Console.WriteLine("Copyright Caroline Malmgren, 2018 :D");
 
+            List<Movie> movies = new List<Movie>();
 
+            movies
+                .OrderBy(m => m.Name);
+
+            foreach (var movie in movies)
+            {
+                Console.WriteLine(movie);
+            }
         }
     }
 }
